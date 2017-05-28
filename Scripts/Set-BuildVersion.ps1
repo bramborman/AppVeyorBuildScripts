49d8a4c84bb022a7bd9a1ef7d9b02d3764c6c1ac
Marian Dolinský <mariandolinsky@outlook.com>
2017-05-28 21:54:06 +0200

    Updated Set-BuildVersion script since build cannot be ommited from AppVeyor version

--- Scripts/Set-BuildVersion.ps1
+++ Scripts/Set-BuildVersion.ps1
@@ -1,4 +1,4 @@
-param([string]$Template = "[Version]-[Branch]_[DateTime]_[Build]")
+param([string]$Template = "[Version]-[Branch]_[DateTime]_[Build]", [string]$Separator = "-")
 Write-Host "`nSet-BuildVersion script executed"
 Write-Host   "================================"
 
@@ -7,7 +7,15 @@ if ([string]::IsNullOrWhiteSpace($Template))
     throw "Parameter `$Template must not be null or white space."
 }
 
-$newBuildVersion = $Template.Replace("[Version]", $env:APPVEYOR_BUILD_VERSION)
+if ([string]::IsNullOrWhiteSpace($Separator))
+{
+    throw "Parameter `$Separator must not be null or white space."
+}
+
+$buildVersion = $env:APPVEYOR_BUILD_VERSION
+$version = $buildVersion.Substring(0, $buildVersion.IndexOf($Separator) - 1)
+
+$newBuildVersion = $Template.Replace("[Version]", $version)
 $newBuildVersion = $newBuildVersion.Replace("[Branch]", $env:APPVEYOR_REPO_BRANCH)
 $newBuildVersion = $newBuildVersion.Replace("[DateTime]", [DateTime]::UtcNow.ToString("yyyy-MM-dd_HH:mm:ss"))
 $newBuildVersion = $newBuildVersion.Replace("[Build]", $env:APPVEYOR_BUILD_VERSION)

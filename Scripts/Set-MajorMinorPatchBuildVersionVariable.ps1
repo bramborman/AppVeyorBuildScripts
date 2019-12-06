35c347047098ae9087faed3c91e603ae48f4917f
Marian Dolinský <mariandolinsky@outlook.com>
2019-12-06 21:02:23 +0100

    Set-SemanticVersionVariable replaced with Set-MajorMinorPatchBuildVersionVariable

---
+++ Scripts/Set-MajorMinorPatchBuildVersionVariable.ps1
@@ -0,0 +1,11 @@
+param([string]$Separator = "-", [ValidateRange(0, [int]::MaxValue)][int]$SplitIndex = 0)
+Write-Host "`nSet-MajorMinorPatchBuildVersionVariable script executed"
+Write-Host   "======================================================="
+
+if ([string]::IsNullOrWhiteSpace($Separator))
+{
+    throw "Parameter `$Separator must not be null or white space."
+}
+
+$semanticVersion = "$($env:APPVEYOR_BUILD_VERSION.Split($Separator)[$SplitIndex])$env:APPVEYOR_BUILD_NUMBER"
+Set-AppveyorBuildVariable "MAJOR_MINOR_PATCH_BUILD_VERSION" $semanticVersion

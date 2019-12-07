42c7bd22d14d37491053b800be891c021590ecf9
Marian Dolinský <mariandolinsky@outlook.com>
2019-12-07 21:02:00 +0100

    Fix for Set-MajorMinorPatchBuildVersionVariable

--- Scripts/Set-MajorMinorPatchBuildVersionVariable.ps1
+++ Scripts/Set-MajorMinorPatchBuildVersionVariable.ps1
@@ -7,5 +7,5 @@ if ([string]::IsNullOrWhiteSpace($Separator))
     throw "Parameter `$Separator must not be null or white space."
 }
 
-$semanticVersion = "$($env:APPVEYOR_BUILD_VERSION.Split($Separator)[$SplitIndex])$env:APPVEYOR_BUILD_NUMBER"
+$semanticVersion = "$($env:APPVEYOR_BUILD_VERSION.Split($Separator)[$SplitIndex]).$env:APPVEYOR_BUILD_NUMBER"
 Set-AppveyorBuildVariable "MAJOR_MINOR_PATCH_BUILD_VERSION" $semanticVersion
